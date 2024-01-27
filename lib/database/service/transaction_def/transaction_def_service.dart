a38d41d59bc34fd230376015a8a3ed5071513499
tkysn10281028 <95330554+tkysn10281028@users.noreply.github.com>
2024-01-27 16:28:31 +0900

    取引定義CRUDの微調整

--- lib/database/service/transaction_def/transaction_def_service.dart
+++ lib/database/service/transaction_def/transaction_def_service.dart
@@ -1,3 +1,4 @@
+import '../../../utils/pair_list.dart';
 import '../../crud/transaction_def/transaction_def_crud.dart';
 import '../../dto/transaction_def/transaction_def_insert_dto.dart';
 import '../../dto/transaction_def/transaction_def_update_sort_order_dto.dart';
@@ -12,22 +13,22 @@ class TransactionDefService {
   // 取得
   //--------------------------
   Future<List<TransactionDefModel>> getResult() async {
-    return transactionDefCrud.getResult();
+    return await transactionDefCrud.getResult();
   }
 
   //--------------------------
   // 登録
   //--------------------------
   Future<void> insert(
-      List<TransactionDefInsertDto> transactionDefDtoList) async {
-    transactionDefCrud.insert(transactionDefDtoList);
+      PairList<TransactionDefInsertDto> transactionDefDtoList) async {
+    await transactionDefCrud.insert(transactionDefDtoList);
   }
 
   //--------------------------
   // 削除
   //--------------------------
   Future<void> delete(String transactionDefId) async {
-    transactionDefCrud.delete(transactionDefId);
+    await transactionDefCrud.delete(transactionDefId);
   }
 
   //--------------------------
@@ -35,6 +36,6 @@ class TransactionDefService {
   //--------------------------
   Future<void> updateSortOrder(TransactionDefUpdateSortOrderDto transactionDef1,
       TransactionDefUpdateSortOrderDto transactionDef2) async {
-    transactionDefCrud.updateSortOrder(transactionDef1, transactionDef2);
+    await transactionDefCrud.updateSortOrder(transactionDef1, transactionDef2);
   }
 }
