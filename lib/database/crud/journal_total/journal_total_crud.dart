4229a0e6f14a91ebfb5609fa92b2bec031ef24f1
tkysn10281028 <95330554+tkysn10281028@users.noreply.github.com>
2024-01-27 13:51:52 +0900

    取引定義のAPI作成・各API内の命名見直し

--- lib/database/crud/journal_total/journal_total_crud.dart
+++ lib/database/crud/journal_total/journal_total_crud.dart
@@ -3,8 +3,9 @@ import 'package:shiwake_app_v2/database/query/journal_total/journal_total_select
 import '../../database_helper.dart';
 
 class JournalTotalCrud {
-  static Future<List<Map<String, dynamic>>> getTJournalTotal(String createYm) async {
+  static Future<List<Map<String, dynamic>>> getResult(String createYm) async {
     final db = await DatabaseHelper.instance.database;
-    return await db.rawQuery(JournalTotalSelectQueries.getTJournalTotal,[createYm]);
+    return await db
+        .rawQuery(JournalTotalSelectQueries.getTJournalTotal, [createYm]);
   }
 }
