c755f5f8515b425bee7e45371e98cc1b70bd74ba
tkysn10281028 <95330554+tkysn10281028@users.noreply.github.com>
2024-02-02 14:50:50 +0900

    画面遷移の動作確認中コンポーネントをコミット

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,60 @@
-
 import 'package:flutter/material.dart';
-import 'package:shiwake_app_v2/screen/common/my_base_color_screen.dart';
-import 'package:shiwake_app_v2/screen/common/my_slide_transition.dart';
+import 'package:shiwake_app_v2/screen/common/my_transition_button.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    home: HomeScreen(),
-  ));
+void main() => runApp(const MaterialApp(
+      title: 'Button Expand Animation Demo',
+      home: FirstScreen(),
+    ));
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final AnimationController controller = AnimationController(
+      duration: const Duration(milliseconds: 700),
+      vsync: Navigator.of(context), // vsyncにナビゲーターを使用
+    );
+
+    return Scaffold(
+        body:
+            Column(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+      MyTransitionButton(
+        secondScreen: SecondScreen(controller: controller),
+        controller: controller,
+      )
+    ]));
+  }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key, required this.controller});
+
+  final AnimationController controller;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue,
       appBar: AppBar(
-        title: const Text('Home Screen'),
+        title: const Text('Second Screen'),
+        elevation: 0,
+        backgroundColor: Colors.blue,
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(MySlideTransition.create(
-                widget: MyBaseColorScreen(color: Colors.purple[100]!),
-                duration: const Duration(milliseconds: 700),
-                curve: Curves.easeInOutSine));
-          },
-          child: const Text('Go to New Screen'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Welcome to Second Screen'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              child: const Text('Go back to First Screen'),
+              onPressed: () async {
+                await controller.reverse();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
         ),
       ),
     );
